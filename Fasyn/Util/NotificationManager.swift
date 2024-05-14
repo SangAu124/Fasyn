5bd3e5bd6e26a219b6b086ebf635183f088386df
ShenKim <dev.sangau20@gmail.com>
2024-05-14 16:58:53 +0900

    🔥 :: 필요 없는 로직 삭제

--- Fasyn/Util/NotificationManager.swift
+++ Fasyn/Util/NotificationManager.swift
@@ -17,40 +17,6 @@ class NotificationManager {
         }
     }
     
-    enum TriggerType: String {
-        case time = "time"
-        case calendar = "calendar"
-        case location = "location"
-        
-        var trigger: UNNotificationTrigger {
-            switch self {
-            case .time:
-                return UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            case .calendar:
-                let dateComponents = DateComponents(hour: 20, minute: 26, weekday: 2)
-                return UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-            case .location:
-                let coordinate = CLLocationCoordinate2D(latitude: 40.0, longitude: 50.0)
-                let region = CLCircularRegion(center: coordinate, radius: 100, identifier: UUID().uuidString)
-                region.notifyOnExit = false
-                region.notifyOnEntry = true
-                return UNLocationNotificationTrigger(region: region, repeats: true)
-            }
-        }
-    }
-    
-    func scheduleNotification(trigger: TriggerType) {
-        let content = UNMutableNotificationContent()
-        content.title = "회의를 시작 할 시간이에요!"
-        content.subtitle = "구성원들에게 연락을 돌려볼까용?"
-        content.sound = .default
-        content.badge = 1
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger.trigger)
-        UNUserNotificationCenter.current().add(request)
-    }
-    
-    
     func scheduleNotification(for meeting: Meeting) {
         let center = UNUserNotificationCenter.current()
         let content = UNMutableNotificationContent()
